a09c7f9fd9c116626904694c1f60fabd22bd68f9
Michael Wilson <wilson.michael.c@gmail.com>
2019-10-02 15:52:12 -0400

    comments

--- isNumber.js
+++ isNumber.js
@@ -1,6 +1,9 @@
 function isNumber(n) {
-  let s = n.replace(/(\r\n|\n|\r|\s)/gm, '');
+  // Remove spaces, newlines, carriage returns, 
+  let s = n.replace(/(\r\n|\n|\r|\t|\s)/gm, '');
+  // Construct a number
   let p = Number(s);
+  // Check if NaN or empty string (which would eval to 0) and return appropriately
   if (isNaN(p) || s === '') {
     // console.log('not a number');
     return false;
